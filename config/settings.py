fc854a4592baa36a9ae200f092c861660b64a095
dori-dev <mr.dori.dev@gmail.com>
2022-07-24 17:10:36 +0430

    add media file

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'medias'
+MEDIA_URL = 'medias/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
